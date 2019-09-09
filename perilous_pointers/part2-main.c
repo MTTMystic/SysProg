aa54d6e8fbbbb428bbd18137514377f818ff91fc
Anasatasia <tdavis52@illinois.edu>
2019-09-09 13:32:20 -0500

    nearly done part2

--- perilous_pointers/part2-main.c
+++ perilous_pointers/part2-main.c
@@ -13,5 +13,37 @@
  */
 int main() {
     // your code here
+    first_step(81);
+
+    int val = 132;
+    
+    second_step(&val);
+
+    val = 8942;
+
+    int * val_arr[1] = {&val};
+
+    double_step(val_arr);
+
+    empty_step("abc");
+
+    two_step("beautiful", "beautiful");
+
+    char * seq = malloc(6);
+    seq[0] = 'a';
+    seq[2] = 'a';
+    seq[3] = 'a';
+
+    three_step(seq, (seq+2), (seq+4));
+    free(seq);
+
+    step_step_step("a@", "aaH", "aaaP");
+
+    it_may_be_odd("a", 97);
+
+    char * immutable = "CS233,CS241,CS357";
+    char * mutable = strdup(immutable);
+
+    tok_step(mutable);
     return 0;
 }
