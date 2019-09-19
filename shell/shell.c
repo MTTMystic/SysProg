ab809bfe57bee1a8a8983206a46cb4106c3cd005
Anasatasia <tdavis52@illinois.edu>
2019-09-19 02:32:56 -0500

    basic command parser + working (?) external exec from stdin

--- shell/shell.c
+++ shell/shell.c
@@ -21,7 +21,49 @@ typedef struct process {
 
 static vector * history;
 
+
+/**
+ * Given a vector which contains arguments given for a command, reduce it to 'real' arguments (not empty strings produced by splitting on whitespace)
+ */
+void trim_args(vector * args) {
+    int arg_idx = vector_size(args) - 1;
+    char * current;
+    for (; arg_idx >= 0; arg_idx--) {
+        current = vector_get(args, arg_idx);
+        if (!strcmp(current, "")) {
+             vector_erase(args, arg_idx);
+        }
+       
+    }
+}
+
+
+/**
+ * Given a string 'command', separate it into multiple arguments
+ */
+char ** parse_command(char * command) {
+    sstring * command_wrapper = cstr_to_sstring(command); //wrap command as sstring to use split
+    vector *  args = sstring_split(command_wrapper, ' '); //separate by spaces
+    trim_args(args);    //remove empty strings created by split on whitespace
+
+    size_t arg_count = vector_size(args);
+
+    size_t bytes_needed = sizeof(char *) * arg_count;
+
+    char ** command_args = malloc(bytes_needed + sizeof(void *));  
+    
+    size_t arg_idx = 0;
+    for (; arg_idx < arg_count; arg_idx++) {
+        command_args[arg_idx] = vector_get(args, arg_idx);
+    }
+    command_args[arg_count + 1] = NULL; 
+    
+    return command_args;
+}
+
 void exec_command(char * command) {
+    char ** command_argv = parse_command(command);
+
     pid_t child_pid = fork();
 
     if (child_pid < 0) {
@@ -32,6 +74,9 @@ void exec_command(char * command) {
         //child process
         pid_t my_pid = getpid();
         print_command_executed(my_pid); //command exec'd by <pid>
+        execvp(command_argv[0], command_argv);
+        print_exec_failed(command);
+        exit(1);
     } else {
         int status;
         waitpid(child_pid, &status, 0);
@@ -39,6 +84,7 @@ void exec_command(char * command) {
 
     
 }
+
 int shell(int argc, char *argv[]) {
     history = string_vector_create();
 
