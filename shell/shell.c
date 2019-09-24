b8f33cad52fd6efbf97a0e063079cd0b500b8568
Anasatasia <tdavis52@illinois.edu>
2019-09-23 21:57:51 -0500

    ummmmmmmmmmm

--- shell/shell.c
+++ shell/shell.c
@@ -9,25 +9,58 @@
 
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+
 typedef struct process {
     char *command;
     pid_t pid;
 } process;
 
-static vector * history;
-static char * history_file_name;
-static char * history_fp;
-static FILE * history_file;
+static vector * history = NULL;
+static char * history_file_name = NULL;
+static char * history_fp = NULL;
+static FILE * history_file = NULL;
 
-static char * script_file_name;
-static char * script_fp;
-static FILE * script_file;
+static char * script_file_name = NULL;
+static char * script_fp = NULL;
+static FILE * script_file = NULL;
 
+#define close_shell_err cleanup(); exit(EXIT_FAILURE);
+#define close_shell cleanup(); exit(EXIT_SUCCESS);
+
+
+void cleanup() {
+    while (waitpid((pid_t) (-1), 0, WNOHANG) > 0);
+    
+    vector_destroy(history);
+
+    if (history_fp) {
+        free(history_fp);
+    }
+
+    if (script_fp) {
+        free(script_fp);
+    }
+}
+
+void sig_handler(int signal) {
+    switch(signal) {
+        case SIGINT: //ignore sigint
+            break;
+        case SIGCHLD:
+            while (waitpid((pid_t) (-1), 0, WNOHANG) > 0); //await children w/o hang (in case multiple finish)
+            break;
+        default:
+            break;
+    }
+}
+
+void command_controller(char * command);
 /**
  * Reads command history from file (if it wasn't created by shell) and stores in internal vector.
  */ 
@@ -59,10 +92,6 @@ void check_history_file(char * opt_arg) {
     } else {
         print_history_file_error();
         history_file = fopen(history_file_name, "w"); //open the history file in write mode to create it
-        if (!history_file) {
-            perror("Error creating history file.");
-            exit(1);
-        }
         fclose(history_file);
     }
 }
@@ -80,7 +109,7 @@ void check_script_file(char * opt_arg) {
     int file_exists = script_file_name && (access(script_file_name, R_OK) != -1);
     if (!file_exists) {
         print_script_file_error();
-        exit(1);
+        close_shell_err
     }
     
 
@@ -102,7 +131,7 @@ void parse_options(int argc, char * argv[]) {
                 break;
             default:
                 print_usage();
-                exit(1);
+                close_shell_err
         }
     }
 }
@@ -137,6 +166,15 @@ void trim_args(vector * args) {
     }
 }
 
+size_t get_argc(char ** argv) {
+    size_t argc = 0;
+    char * arg;
+    while ((arg = argv[argc]) != NULL) {
+        argc++;
+    }
+
+    return argc;
+}
 /**
  * Given a string 'command', separate it into distinct arguments and place in string array
  */
@@ -166,12 +204,15 @@ char ** parse_command(char * command) {
  * TODO: add support for backgrounding
  */ 
 
-void exec_command(char * command) {
-    record_command(command); //add command to history
-    char ** command_argv = parse_command(command);
+void exec_command(char ** command_argv, int argc) {
+    int background = argc >= 2 && !strcmp(command_argv[argc - 1], "&");
+    //delete the & character so exec doesn't treat it as an argument
+    if (background) {
+        command_argv[argc - 1] = NULL;
+    }
 
     pid_t child_pid = fork();
-
+    
     if (child_pid < 0) {
         //fork failed
         print_fork_failed();
@@ -181,25 +222,114 @@ void exec_command(char * command) {
         pid_t my_pid = getpid();
         print_command_executed(my_pid); //command exec'd by <pid>
         execvp(command_argv[0], command_argv);
-        print_exec_failed(command);
-        exit(1);
-    } else {
-        int status;
-        waitpid(child_pid, &status, 0);
+        print_exec_failed(command_argv[0]); //exec failed if this code is reached
+        close_shell_err
+    } else if (!background) {
+         int status;
+         waitpid(child_pid, &status, 0);
     }
-
     
 }
 
+void exec_cd(char ** command_argv, int argc) {
+    if (argc < 2) {
+        print_no_directory("");
+    }
+
+    char * path = command_argv[1];
+    int chdir_result = chdir(path);
+    if (chdir_result < 0) {
+        print_no_directory(path);
+    }
+}
+
+void list_history() {
+    size_t idx = 0;
+    for (; idx < vector_size(history); idx++) {
+        char * history_line = vector_get(history, idx);
+        print_history_line(idx, history_line);
+    }
+}
+
+void search_by_index(const char * command) {
+    if(strlen(command) < 2) {
+        print_invalid_command(command);
+        return;
+    }
+
+    size_t n = atoi(command + 1);
+
+    if (n > vector_size(history) - 1) {
+        print_invalid_index();
+        return;
+    }
+
+    char * history_line = vector_get(history, n);
+    print_command(history_line);
+    command_controller(history_line);
+}
+
+void search_by_prefix(const char * command) {
+    int prefix_length = strlen(command) - 1;
+    if (prefix_length == 0) {
+        search_by_index("#0");
+    } else {
+        size_t idx = 0;
+        while (idx < vector_size(history)) {
+            char * current_line = vector_get(history, idx);
+            sstring * current_line_wrapper = cstr_to_sstring(current_line);
+            char * current_prefix = sstring_slice(current_line_wrapper, 0, prefix_length);
+
+            if (!strcmp(current_prefix, command + 1)) {
+                print_command(current_line);
+                command_controller(current_line);
+                return;
+            }
+
+            idx++;
+        }
+
+        if (idx >= vector_size(history)) {
+            print_no_history_match();
+        }
+    }
+}
+
+void command_controller(char * command) {
+    char ** command_argv = parse_command(command);
+    char * program_name = command_argv[0];
+    int argc = get_argc(command_argv);
+
+    int not_print = program_name[0] == '!' || program_name[0] == '#';
+    if (!not_print) record_command(command);
+
+    if (!strcmp(program_name, "cd")) {
+        exec_cd(command_argv, argc);
+    } else if (!strcmp(program_name, "!history")) {
+        list_history();
+    } else if (program_name[0] == '#') {
+        search_by_index(program_name);
+    } else if (program_name[0] == '!') {
+        search_by_prefix(program_name);
+    } else if (!strcmp(program_name, "exit")) {
+        close_shell;
+     } else {
+        exec_command(command_argv, argc);
+    }
+    
+}
 
 void main_loop() {
+    printf("in main loop\n");
     if (script_file_name) {
         script_file = fopen(script_fp, "r");
         if (!script_file) {
             print_script_file_error();
-            exit(1);
+            close_shell_err
         }
 
+    }
+        printf("past script file check\n");
         FILE * command_src = script_file ? script_file : stdin;
 
         pid_t shell_pid  = getpid();
@@ -212,7 +342,7 @@ void main_loop() {
             }
 
             if (!fgets(line, sizeof(line), command_src)) {
-                break; //TODO: cleanup functions
+                 close_shell
             }
 
             char * final_char = strchr(line, '\n');
@@ -220,19 +350,23 @@ void main_loop() {
                 *final_char = '\0';
             }
 
+            
             if (command_src == script_file) {
                 print_command(line);
             }
 
-            exec_command(line);
+            command_controller(line);
 
 
         }
 
-        fclose(script_file);
-    }
+    fclose(script_file);
 }
 int shell(int argc, char *argv[]) {
+
+    signal(SIGINT, sig_handler);
+    signal(SIGCHLD, sig_handler);
+
     history = string_vector_create();
     parse_options(argc, argv);
 
