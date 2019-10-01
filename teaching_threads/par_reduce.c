3a489d1489d71303b14d44556ef2df152d7570f9
Anasatasia <tdavis52@illinois.edu>
2019-09-30 21:29:48 -0500

    task struct + start routine

--- teaching_threads/par_reduce.c
+++ teaching_threads/par_reduce.c
@@ -12,11 +12,27 @@
 #include "reducers.h"
 
 /* You might need a struct for each task ... */
+typedef struct task {
+    pthread_t id;
+    reducer r_func;
+    int base_case;
+    int * list;
+    size_t list_len;
+    int result;
 
+} task;
 /* You should create a start routine for your threads. */
+void sub_reduce(task * data) {
+    if (!data) return;
 
+    data->result = reduce(data->list, data->list_len, data->r_func, data->base_case);
+}
 int par_reduce(int *list, size_t list_len, reducer reduce_func, int base_case,
                size_t num_threads) {
     /* Your implementation goes here */
+    task * tasks = calloc(num_threads, sizeof(task));
+
+    free(tasks);
+    
     return 0;
 }
