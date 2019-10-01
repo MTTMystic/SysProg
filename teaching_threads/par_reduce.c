537450e5eaf7e645de0dea80bfa9dd97fca2fdff
Anasatasia <tdavis52@illinois.edu>
2019-09-30 21:59:01 -0500

    tentative thread creation?

--- teaching_threads/par_reduce.c
+++ teaching_threads/par_reduce.c
@@ -22,16 +22,36 @@ typedef struct task {
 
 } task;
 /* You should create a start routine for your threads. */
-void sub_reduce(task * data) {
-    if (!data) return;
+void * sub_reduce(task * data) {
+    if (!data) return NULL;
 
     data->result = reduce(data->list, data->list_len, data->r_func, data->base_case);
+    return NULL;
 }
+
+void initialize_task(task * t, reducer r_func, int base_case, int * list, size_t len) {
+    t->r_func = r_func;
+    t->base_case = base_case;
+    t->list = list;
+    t->list_len = len;
+}
+
 int par_reduce(int *list, size_t list_len, reducer reduce_func, int base_case,
                size_t num_threads) {
     /* Your implementation goes here */
     task * tasks = calloc(num_threads, sizeof(task));
+    size_t thread_idx = 0;
+    size_t default_sub_len = list_len / num_threads;
+    for (; thread_idx < num_threads; thread_idx++) {
+        size_t offset_idx = default_sub_len * thread_idx;
+        size_t sub_len = default_sub_len;
+        if (thread_idx == num_threads - 1) {
+            sub_len = list_len - (default_sub_len * thread_idx);
+        }
 
+        initialize_task(&tasks[thread_idx], reduce_func, base_case, list + offset_idx, sub_len);
+        pthread_create(&tasks[thread_idx].id, NULL, (void * ) sub_reduce, &tasks[thread_idx]);
+    }
     free(tasks);
     
     return 0;
