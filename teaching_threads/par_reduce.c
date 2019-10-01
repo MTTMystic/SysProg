b684c38a8b774897e20b15acf32d1f7e7066b6b3
Anasatasia <tdavis52@illinois.edu>
2019-10-01 02:29:10 -0500

    local tests working

--- teaching_threads/par_reduce.c
+++ teaching_threads/par_reduce.c
@@ -29,30 +29,48 @@ void * sub_reduce(task * data) {
     return NULL;
 }
 
-void initialize_task(task * t, reducer r_func, int base_case, int * list, size_t len) {
-    t->r_func = r_func;
-    t->base_case = base_case;
-    t->list = list;
-    t->list_len = len;
+void initialize_tasks(task * t, reducer r_func, int base_case, 
+                        int * list, size_t len, size_t num_threads) {
+    
+    size_t default_sub_len = len / num_threads;
+    
+    size_t idx = 0;
+    for(; idx < num_threads; idx++) {
+        size_t offset = (default_sub_len * idx);
+        size_t sub_len = (idx < num_threads - 1) ? default_sub_len : len - offset;
+        
+        t[idx].list = list + offset;
+        t[idx].list_len = sub_len;
+        t[idx].r_func = r_func;
+        t[idx].base_case = base_case;
+        printf("first: %d\n", t[idx].list[0]);
+        
+        
+    }
 }
 
 int par_reduce(int *list, size_t list_len, reducer reduce_func, int base_case,
                size_t num_threads) {
     /* Your implementation goes here */
     task * tasks = calloc(num_threads, sizeof(task));
-    size_t thread_idx = 0;
-    size_t default_sub_len = list_len / num_threads;
+    initialize_tasks(tasks, reduce_func, base_case, list, list_len, num_threads);
+   
+   
+    size_t thread_idx = 0;    
     for (; thread_idx < num_threads; thread_idx++) {
-        size_t offset_idx = default_sub_len * thread_idx;
-        size_t sub_len = default_sub_len;
-        if (thread_idx == num_threads - 1) {
-            sub_len = list_len - (default_sub_len * thread_idx);
-        }
-
-        initialize_task(&tasks[thread_idx], reduce_func, base_case, list + offset_idx, sub_len);
         pthread_create(&tasks[thread_idx].id, NULL, (void * ) sub_reduce, &tasks[thread_idx]);
     }
+    
+    int result[num_threads];
+    size_t result_idx = 0;
+
+    for(; result_idx < num_threads; result_idx++) {
+        pthread_join(tasks[result_idx].id, NULL);
+        result[result_idx] = tasks[result_idx].result;
+    }
+
+    int final_result = reduce(result, num_threads, reduce_func, 0);
     free(tasks);
     
-    return 0;
+    return final_result;
 }
