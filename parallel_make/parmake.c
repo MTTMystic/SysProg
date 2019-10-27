a564d476778f3dd82a207dcdbb09249227606c91
Anasatasia <tdavis52@illinois.edu>
2019-10-27 17:19:07 -0500

    passing part1 testcases

--- parallel_make/parmake.c
+++ parallel_make/parmake.c
@@ -8,16 +8,21 @@
 #include "graph.h"
 #include "parmake.h"
 #include "parser.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 
-static graph * dep_graph;
 
+static graph * dep_graph;
 
 /**Status numbers meanings
  * 0: Rule unsatisfied
  * 1: Rule satisfied
- * 2: Rule dropped due to cyclical dependency
- * 
+ * 2: Rule dropped
+ * 3: Rule failed
  * */
 //cycle detection code based on algorithm found at geeksforgeeks and adapted for this purpose
 
@@ -62,9 +67,6 @@ int cycleHelper(char * target, int t_idx, vector * targets, char visited[], char
 int detectCycle(char * target) {
     vector * targets = graph_vertices(dep_graph);
     size_t n_targets = vector_size(targets);
-
-    printf("n targets is: %zu\n", n_targets);
-
     char visited[n_targets];
     char rec_stack[n_targets];
 
@@ -74,38 +76,132 @@ int detectCycle(char * target) {
     }
 
     int t_idx = vector_idx_of(targets, target);
-    printf("idx of this target is: %d\n", t_idx);
-    return cycleHelper(target, t_idx, targets, visited, rec_stack);
+    int hasCycle = cycleHelper(target, t_idx, targets, visited, rec_stack);
+    vector_destroy(targets);
+    return hasCycle;
+}
+
+int is_fod(rule_t * rule) {
+    return access(rule->target, R_OK) != -1;
+}
+
+int depends_on_fod(rule_t * rule) {
+    vector * deps = graph_neighbors(dep_graph, rule->target);
+    for (size_t d_idx = 0; d_idx < vector_size(deps); d_idx++) {
+        char * dep_target = vector_get(deps, d_idx);
+        rule_t * dep_rule = (rule_t *) graph_get_vertex_value(dep_graph, dep_target);
+        if (!is_fod(dep_rule)) {
+            return 0;
+        }
+    }
+
+    vector_destroy(deps);
+    return 1;
+}
+
+int newer_file_change(rule_t * rule) {
+    vector * deps = graph_neighbors(dep_graph, rule->target);
+    struct stat rule_stat;
+    struct stat dep_stat;
+
+    stat(rule->target, &rule_stat);
+
+    struct timespec rule_mtim = rule_stat.st_mtim;
+
+    char dep_newer = 0;
+    for (size_t dep_idx = 0; dep_idx < vector_size(deps); dep_idx++) {
+        char * target = vector_get(deps, dep_idx);
+        rule_t * dep_rule = (rule_t *) graph_get_vertex_value(dep_graph, target);
+
+        stat(dep_rule->target, &dep_stat);
+
+        struct timespec dep_mtim = dep_stat.st_mtim;
+
+        dep_newer = (rule_mtim.tv_sec < dep_mtim.tv_sec) || (rule_mtim.tv_nsec < dep_mtim.tv_nsec);
+
+        if (dep_newer) {
+            return 1;
+        }
+    
+    }
+
+    return 0;
 }
 
 void satisfy_rule(rule_t * rule) {
-    //figure out if this rule depends on any other rules
-    vector * depends = graph_neighbors(dep_graph, rule->target);
-    //check for a cycle in the graph involving this rule
-    if (detectCycle(rule->target)) {
-        vector_destroy(depends);
-        rule->state = 2;
-        return;
+    
+    /*Rule should only run again when:
+        1) It is the name of a file on disk.
+        2) It only depends on other files on disk.
+        3) One of its dependencies has been updated more recently than it has.
+    */
+    if (is_fod(rule) && depends_on_fod(rule)) {
+        int run_again = newer_file_change(rule);
+        rule->state = !run_again;    //mark rule as unsatisfied if necessary
     }
+
+    if (rule->state == 1 || rule->state == 3) {
+        return;     //don't satisfy rules already ran and/or failed
+    }
+
+
+    //recursively satisfy this rule's dependencies
+    vector * deps = graph_neighbors(dep_graph, rule->target);
+    for(size_t d_idx = 0; d_idx < vector_size(deps); d_idx++) {
+        char * dep_target = vector_get(deps, d_idx);
+        rule_t * dep_rule = (rule_t *) graph_get_vertex_value(dep_graph, dep_target);
+        
+        satisfy_rule(dep_rule);
+
+        //do not satisfy dep rules that have already failed
+        if (dep_rule->state == 3) {
+            rule->state = 3;
+        }  
+    }
+
+    vector_destroy(deps);
+
+    size_t c_idx = 0;
+    char * command;
+
+    while (rule->state != 3 && c_idx < vector_size(rule->commands)) {
+        command = vector_get(rule->commands, c_idx);
+        int command_result = system(command);
+        if (command_result != 0) {
+            rule->state = 3;
+            return;
+        }
+
+        c_idx++;
+    }
+
+    rule->state = 1;
+    return;    
 }
 
+void satisfy_goal_rules() {
+    vector * goal_rules = graph_neighbors(dep_graph, ""); //get goal rules
+    for (size_t g_idx = 0; g_idx < vector_size(goal_rules); g_idx++) {
+        char * target = (char *) vector_get(goal_rules, g_idx);
+        rule_t * rule = graph_get_vertex_value(dep_graph, target);
+        int hasCycle = detectCycle(target);
+        if (hasCycle) {
+            rule->state = 2; //skip goal rules in cycle (or whose dependents are)
+            print_cycle_failure(target);
+            continue;
+        }
+        satisfy_rule(rule);
+    }
+
+    vector_destroy(goal_rules);
+}
 int parmake(char *makefile, size_t num_threads, char **targets) {
     // good luck!
 
     //parse makefile to get dependency graph
     dep_graph = parser_parse_makefile(makefile, targets);
 
-    vector * goal_rules = graph_neighbors(dep_graph, "");
-    for (size_t r_idx = 0; r_idx < vector_size(goal_rules); r_idx++) {
-        char * target = vector_get(goal_rules, r_idx);
-        rule_t * rule = (rule_t *) graph_get_vertex_value(dep_graph, target);
-        satisfy_rule(rule);
-        //announce if a goal rule is dropped because of cyclical dependencies
-        if (rule->state == 2) {
-            print_cycle_failure(target);
-        }
-
-    }   
+    satisfy_goal_rules();
 
     return 0;
 }
