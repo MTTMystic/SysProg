b6aa0d59b11d8622aae6d7038c59f09042e13831
Anasatasia <tdavis52@illinois.edu>
2019-11-04 19:05:36 -0600

    kinks to work out

--- parallel_make/.objs/parmake-release.d
+++ parallel_make/.objs/parmake-release.d
@@ -1,6 +1,6 @@
 .objs/parmake-release.o: parmake.c format.h includes/graph.h \
  includes/callbacks.h includes/compare.h includes/dictionary.h \
- includes/vector.h parmake.h parser.h rule.h
+ includes/vector.h parmake.h parser.h rule.h includes/set.h
 
 format.h:
 
@@ -19,3 +19,5 @@ parmake.h:
 parser.h:
 
 rule.h:
+
+includes/set.h:
--- parallel_make/parmake.c
+++ parallel_make/parmake.c
@@ -8,21 +8,29 @@
 #include "graph.h"
 #include "parmake.h"
 #include "parser.h"
+#include "set.h"
+
 #include <unistd.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
-
+#include <pthread.h>
 
 static graph * dep_graph;
+static set * rules;
+static pthread_mutex_t rules_lock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t  rule_cv = PTHREAD_COND_INITIALIZER;
+
+static pthread_t * workers;
 
 /**Status numbers meanings
+ * -1: Rule failed
  * 0: Rule unsatisfied
  * 1: Rule satisfied
- * 2: Rule dropped
- * 3: Rule failed
+ * 3: Rule ready to run
+ * 4: Rule running
+ * 
  * */
 //cycle detection code based on algorithm found at geeksforgeeks and adapted for this purpose
 
@@ -42,23 +50,28 @@ int cycleHelper(char * target, int t_idx, vector * targets, char visited[], char
         visited[t_idx] = 1;
         rec_stack[t_idx] = 1;
 
+        rule_t * rule = (rule_t *) graph_get_vertex_value(dep_graph, target);
         vector * neighbors = graph_neighbors(dep_graph, target);
 
         for (size_t n_idx = 0; n_idx < vector_size(neighbors); n_idx++) {
             char * neighbor = (char *) vector_get(neighbors, n_idx);
             int n_vidx = vector_idx_of(targets, neighbor);
             char already_visited = visited[n_vidx];
-            
+            //rule_t * n_rule = (rule_t *) graph_get_vertex_value(dep_graph, neighbor);
+
             if (!already_visited && cycleHelper(neighbor, n_vidx, targets, visited, rec_stack)) {
+                rule->state = -1;
                 vector_destroy(neighbors);
                 return 1;
             } else if (rec_stack[n_vidx]) {
+                rule->state = -1;
                 vector_destroy(neighbors);
                 return 1;
             }
             
         }
 
+        rule->state = 0;
         vector_destroy(neighbors);
     }
 
@@ -134,72 +147,192 @@ int newer_file_change(rule_t * rule) {
     return 0;
 }
 
-void satisfy_rule(rule_t * rule) {
-    
-    /*Rule should only run again when:
-        1) It is the name of a file on disk.
-        2) It only depends on other files on disk.
-        3) One of its dependencies has been updated more recently than it has.
-    */
-    if (is_fod(rule) && depends_on_fod(rule)) {
-        int run_again = newer_file_change(rule);
-        rule->state = !run_again;    //mark rule as unsatisfied if necessary
+
+void parse_targets_helper(char * target) {
+    rule_t * rule = (rule_t *) graph_get_vertex_value(dep_graph, target);
+
+    if (rule->state == -1) {
+        return; 
     }
 
-    if (rule->state == 1 || rule->state == 3) {
-        return;     //don't satisfy rules already ran and/or failed
+    set_add(rules, rule);
+    vector * neighbors = graph_neighbors(dep_graph, target);
+    for (size_t n_idx = 0; n_idx < vector_size(neighbors); n_idx++) {
+        char * neighbor = (char *) vector_get(neighbors, n_idx);
+        rule_t * n_rule = (rule_t *) graph_get_vertex_value(dep_graph, neighbor);
+        set_add(rules, n_rule);
+        parse_targets_helper(neighbor);
     }
 
+    vector_destroy(neighbors);
+}
 
-    //recursively satisfy this rule's dependencies
+void parse_targets() {
+    vector * goal_rules = graph_neighbors(dep_graph, "");
+    for (size_t idx = 0; idx < vector_size(goal_rules); idx++) {
+        char * target = (char *) vector_get(goal_rules, idx);
+        parse_targets_helper(target);
+    }
+
+    vector_destroy(goal_rules); 
+}
+
+
+int mark_cyclic(char * target) {
+    int isCyclic = detectCycle(target);
+    if (isCyclic) {
+        rule_t * rule = graph_get_vertex_value(dep_graph, target);
+        rule->state = -1;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void drop_cyclic_goals() {
+    vector * goals = graph_neighbors(dep_graph, "");
+    for (size_t idx = 0; idx < vector_size(goals); idx++) {
+        char * target = vector_get(goals, idx);
+        int cyclic = mark_cyclic(target);
+        if (cyclic) {
+            print_cycle_failure(target);
+        }
+    }
+}
+
+void clear_rules() {
+    vector * rules_vec = set_elements(rules);
+    for (size_t idx = 0; idx < vector_size(rules_vec); idx++) {
+        rule_t * rule = (rule_t *) vector_get(rules_vec, idx);
+        if (rule->state != 0 && rule->state != 3) {
+            set_remove(rules, rule);
+            //printf("Removed %s from rules.\n", rule->target);
+        }
+    }
+}
+
+void check_rule(rule_t * rule) {
+    if (rule->state != 0) {
+        return;
+    }
+
+    //printf("This is a call to graph_neighbors for %s.\n", rule->target);
     vector * deps = graph_neighbors(dep_graph, rule->target);
-    for(size_t d_idx = 0; d_idx < vector_size(deps); d_idx++) {
-        char * dep_target = vector_get(deps, d_idx);
-        rule_t * dep_rule = (rule_t *) graph_get_vertex_value(dep_graph, dep_target);
-        
-        satisfy_rule(dep_rule);
+    //printf("Yep! This call to graph_neighbors for %s worked.\n", rule->target);
+    int failed_dep = 0;
+    int deps_satisfied = 1;
+    for (size_t d_idx = 0; d_idx < vector_size(deps); d_idx++) {
+        char * dep = (char *) vector_get(deps, d_idx);
+        rule_t * d_rule = (rule_t *) graph_get_vertex_value(dep_graph, dep);
 
-        //do not satisfy dep rules that have already failed
-        if (dep_rule->state == 3) {
-            rule->state = 3;
-        }  
+        if (d_rule->state == -1) {
+            failed_dep = 1;
+            break;
+        }
+
+        if (d_rule->state != 1) {
+            deps_satisfied = 0;
+            break;
+        }
+    }
+
+    if (failed_dep) {
+        rule->state = -1;
+    } else if (deps_satisfied) {
+        rule->state = 3;
+    } else {
+        rule->state = 0;
     }
 
     vector_destroy(deps);
+}
 
-    size_t c_idx = 0;
-    char * command;
+rule_t * find_rule() {
+    vector * rules_vec = set_elements(rules);
+    for (size_t idx = 0; idx < vector_size(rules_vec); idx++) {
+        rule_t * rule = (rule_t *) vector_get(rules_vec, idx);
+        check_rule(rule);
+        if (rule->state == 3) {
+            vector_destroy(rules_vec);
+            return rule;
+        }
+    }
 
-    while (rule->state != 3 && c_idx < vector_size(rule->commands)) {
-        command = vector_get(rule->commands, c_idx);
-        int command_result = system(command);
-        if (command_result != 0) {
+    vector_destroy(rules_vec);
+    return NULL;
+}
+
+void execute_rule(rule_t * rule) {
+    rule->state = 4;
+
+    if (is_fod(rule) && depends_on_fod(rule)) {
+        if (!newer_file_change(rule)) {
             rule->state = 3;
             return;
         }
+    }
 
-        c_idx++;
+    for (size_t c_idx = 0; c_idx < vector_size(rule->commands); c_idx++) {
+        char * command = (char *) vector_get(rule->commands, c_idx);
+        int ret_val = system(command);
+        if (ret_val != 0) {
+            rule->state = -1;
+            return;
+        }
     }
 
     rule->state = 1;
-    return;    
 }
 
-void satisfy_goal_rules() {
-    vector * goal_rules = graph_neighbors(dep_graph, ""); //get goal rules
-    for (size_t g_idx = 0; g_idx < vector_size(goal_rules); g_idx++) {
-        char * target = (char *) vector_get(goal_rules, g_idx);
-        rule_t * rule = graph_get_vertex_value(dep_graph, target);
-        int hasCycle = detectCycle(target);
-        if (hasCycle) {
-            rule->state = 2; //skip goal rules in cycle (or whose dependents are)
-            print_cycle_failure(target);
-            continue;
-        }
-        satisfy_rule(rule);
-    }
+void worker_exit() {
+   int remaining = set_cardinality(rules);
 
-    vector_destroy(goal_rules);
+   if (remaining == 0) {
+       pthread_cond_broadcast(&rule_cv);
+       pthread_mutex_unlock(&rules_lock);
+       pthread_exit(0);
+   } 
+}
+
+void worker_loop() {
+    while(1) {
+        pthread_mutex_lock(&rules_lock);
+        rule_t * exec_rule = NULL;
+        
+        while (1) {
+            worker_exit();
+            size_t idx = 0;
+            vector * rules_vec = set_elements(rules);
+            for(size_t r_idx = 0; r_idx <  vector_size(rules_vec); r_idx++) {
+                rule_t * current = vector_get(rules_vec, idx);
+                check_rule(current);
+
+                if (current->state == -1) {
+                    set_remove(rules, current);
+                } else if (current->state == 0) {
+                    idx++;
+                } else if (current->state == 1) {
+                    set_remove(rules, current);
+                } else if (current->state == 3 ) {
+                    exec_rule = current;
+                    break;
+                } else if (current->state == 4) {
+                    set_remove(rules, current);
+                }
+            }
+
+            if (exec_rule == NULL) {
+                worker_exit();
+                pthread_cond_wait(&rule_cv, &rules_lock);
+            } else {
+                break;
+            }
+        }
+
+        pthread_mutex_unlock(&rules_lock);
+        execute_rule(exec_rule);
+        pthread_cond_broadcast(&rule_cv);
+    }
 }
 
 int parmake(char *makefile, size_t num_threads, char **targets) {
@@ -208,8 +341,24 @@ int parmake(char *makefile, size_t num_threads, char **targets) {
     //parse makefile to get dependency graph
     dep_graph = parser_parse_makefile(makefile, targets);
 
-    satisfy_goal_rules();
+    rules = shallow_set_create();
+    workers = calloc(num_threads, sizeof(pthread_t));
+
+    drop_cyclic_goals();
+    parse_targets();
+    
+    for (size_t idx = 0; idx < num_threads; idx++) {
+        pthread_create(&workers[idx], NULL, (void *) worker_loop, NULL);
+    }
+
+    for (size_t i = 0; i < num_threads; i++) {
+        pthread_join(workers[i], NULL);
+    }
 
     graph_destroy(dep_graph);
+    set_destroy(rules);
+    pthread_mutex_destroy(&rules_lock);
+    pthread_cond_destroy(&rule_cv);
+
     return 0;
 }
