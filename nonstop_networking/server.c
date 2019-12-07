f5d4fd31f94ae2bb498dd3a66273201be6e1cfda
Anasatasia <tdavis52@illinois.edu>
2019-12-06 23:05:20 -0600

    encountered file error for GET request

--- nonstop_networking/server.c
+++ nonstop_networking/server.c
@@ -47,9 +47,9 @@ typedef struct _connection_t {
 } connection_t;
 
 
-static char * temp_dir; //folder name of temp dir
+static char * temp_dir = NULL; //folder name of temp dir
 static vector * file_list; //global list of files
-static dictionary * connections; //global list of client info
+static vector * connections; //global list of client info
 
 static bool endSession; //flag to exit server automata
 static volatile int serverSocket; //socket for incoming connections
@@ -57,22 +57,23 @@ static volatile int serverSocket; //socket for incoming connections
 static volatile int epoll_fd;
 static int clientCount;
 
+void close_connection(connection_t * client_data);
 
 /**--------------------------HOUSEKEEPING & UTILITY------------------------*/
+
 void cleanup() {
-    if (file_list) {
+    if (file_list && !vector_empty(file_list)) {
         vector_destroy(file_list);
     }
 
     if (connections) {
-        vector * conn_t_vec = dictionary_values(connections);
-        for (size_t idx = 0; idx < vector_size(conn_t_vec); idx++) {
-            connection_t * conn_t = vector_get(conn_t_vec, idx);
-            shutdown(conn_t->fd, SHUT_RDWR);
-            close(conn_t->fd);
-            free(conn_t);
-        }
-        dictionary_destroy(connections);
+       for (int i = vector_size(connections) - 1; i >= 0; i--) {
+           if (vector_get(connections, i)) {
+               free(vector_get(connections, i));
+           }
+       }
+
+       vector_destroy(connections);
     }
 
     if (temp_dir) {
@@ -93,6 +94,7 @@ void exit_fail() {
     close_server(SIGINT);
     exit(1);
 }
+
 
 verb string_to_verb(char * m) {
     if (!strcmp(m, "GET")) {
@@ -182,8 +184,12 @@ connection_t * connection_setup(int fd) {
 
     new_conn->fd = fd;
     new_conn->stat = PENDING;
-    new_conn->buf[1024] = 0;
-    new_conn->header[1024] = 0;
+
+    memset(new_conn->buf, 0, sizeof(new_conn->buf));
+    memset(new_conn->header, 0, sizeof(new_conn->header));
+    memset(new_conn->fs_buf, 0, sizeof(new_conn->fs_buf));
+    memset(new_conn->filename, 0, sizeof(new_conn->filename));
+
     new_conn->file_size = pow(2, 8) - 1;
     new_conn->header_bytes = 0;
     new_conn->buf_offset = 0;
@@ -191,7 +197,10 @@ connection_t * connection_setup(int fd) {
     new_conn->closed_write = false;
     new_conn->file_offset = 0;
     new_conn->read_fs = false;
-    dictionary_set(connections, (void *)&fd, (void *)new_conn);
+    new_conn->bytes_read = 0;
+    new_conn->bytes_written = 0;
+
+    vector_push_back(connections, new_conn);
 
     return new_conn;
 }
@@ -228,12 +237,34 @@ void accept_connections() {
    } while (new_cli_fd != -1);
 }
 
+
+void close_connection(connection_t * client_data) {
+    LOG("closing connection??");
+    for (size_t i = 0; i < vector_size(connections); i++) {
+        if (vector_get(connections, i) == client_data) {
+            vector_erase(connections, i);
+            break;
+        }
+    }
+    //stop listening for events
+    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_data->fd, NULL);
+    //shutdown client socket
+    shutdown(client_data->fd, SHUT_RDWR);
+    //close socket
+    close(client_data->fd);
+    //free
+    if (client_data) {
+        free(client_data);
+    }
+    
+}
 /**------------------SOCKET READS/WRITES------------*/
 /*attempts to read up to [n_bytes] bytes from socket into given buffer
  ASSUMES that buf has enough room for n_bytes + 1 (null terminator)*/
 int read_from_socket(connection_t * client_data, char * buf, size_t n_bytes) {
     size_t bytes_read = 0;
-
+    
+    LOG("bytes read was: %zu", client_data->bytes_read);
     bool no_data_left = false;
 
     while (bytes_read < n_bytes) {
@@ -264,6 +295,7 @@ int read_from_socket(connection_t * client_data, char * buf, size_t n_bytes) {
 
     LOG("read %zu bytes from %d", bytes_read, client_data->fd);
     client_data->bytes_read += bytes_read;
+    LOG("bytes_read is: %zu", client_data->bytes_read);
     if (buf == client_data->buf) {
         client_data->buf_offset += bytes_read;
     }
@@ -290,6 +322,7 @@ void read_header(connection_t * client_data) {
 
 bool read_filesize(connection_t * client_data) {
     size_t bytes_read = 0;
+    LOG("parsing filesize");
     while (bytes_read < sizeof(size_t)) {
         bytes_read = read_from_socket(client_data, client_data->fs_buf, 8 - bytes_read);
         
@@ -344,9 +377,10 @@ int write_to_socket(connection_t * client_data, char * buf, size_t n_bytes) {
     return bytes_written;
 }
 
-/**----------------REQUEST HANDLING------------*/
+/**---------------REQUEST HANDLERS--------------*/
 void move_excess_header(connection_t * client_data) {
     size_t excess_bytes = client_data->bytes_read - client_data->header_bytes;
+
     char * excess_buf = client_data->header + client_data->header_bytes;
     //only try to move filesize if the request was PUT and there's enough bytes
     bool get_filesize = client_data->method == PUT && excess_bytes >= sizeof(size_t);
@@ -367,9 +401,11 @@ void move_excess_header(connection_t * client_data) {
         client_data->buf_offset += excess_bytes;
     }
 
+    LOG("bytes_read after moving excess header: %zu", client_data->bytes_read)
 }
 
 void path_append_temp_dir(connection_t * client_data) {
+
     int path_len = strlen(temp_dir) + strlen(client_data->filename) + 1;
     char new_fn[path_len + 1];
     strncpy(new_fn, temp_dir, strlen(temp_dir));
@@ -415,12 +451,14 @@ void parse_header(connection_t * client_data) {
 
     if (client_data->method != LIST) {
         size_t fn_len = cbnl - verb_len - 1;
+
         //LOG("length of filename : %zu", fn_len);
         char filename[fn_len + 1];
         strncpy(filename, client_data->header + verb_len + 1, fn_len);
         filename[fn_len] = 0;
 
-        //LOG("parsed filename is: %s", filename);
+        LOG("parsed filename is: %s", filename);
+
         //move excess header bytes to appropriate buffers
         move_excess_header(client_data);
 
@@ -429,6 +467,7 @@ void parse_header(connection_t * client_data) {
         client_data->stat = FILE_CHECK;
         
         path_append_temp_dir(client_data);
+        
         return;
     }
     
@@ -464,6 +503,7 @@ int create_file(char * filename) {
 }
 
 void check_file(connection_t * client_data) {
+
     if (client_data->method == GET || client_data->method == DELETE) {
         int valid_file = file_exists(client_data->filename);
         if (valid_file == -1) {
@@ -503,13 +543,13 @@ void parse_fs(connection_t * client_data) {
         }
     }
 
+    
     for (size_t idx = 0; idx < sizeof(size_t); idx++) {
         char c = client_data->fs_buf[idx];
         char * target = (char *)(&client_data->file_size) + idx;
         *target = c;
     }
 
-    LOG("filesize is: %zu", client_data->file_size);
     client_data->read_fs = true;
     
     client_data->stat = DATA_READ;
@@ -537,6 +577,7 @@ size_t write_buffer_to_file(connection_t * client_data) {
         perror("lseek(): ");
         return 0;
     }
+
     while (bytes_written < client_data->buf_offset) {
         int retval = write(file_fd, client_data->buf, client_data->buf_offset);
         if (retval == -1) {
@@ -547,7 +588,7 @@ size_t write_buffer_to_file(connection_t * client_data) {
         bytes_written += retval;
     }
 
-    LOG("wrote %zu bytes to file", bytes_written);
+    //LOG("wrote %zu bytes to file", bytes_written);
     //LOG("buffer offset was: %zu", client_data->buf_offset);
 
     if (client_data->buf_offset - bytes_written > 0) {
@@ -576,7 +617,7 @@ void handle_put_request(connection_t * client_data) {
         write_buffer_to_file(client_data);
     }
     int retval = 0;
-
+    LOG("handling put request for some reason???");
     do {
         retval = read_from_socket(client_data, client_data->buf, buf_size - 1);
         write_buffer_to_file(client_data);
@@ -600,6 +641,15 @@ void handle_put_request(connection_t * client_data) {
     }
 }
 
+void send_fs(connection_t * client_data){
+    int file_fd = open(client_data->filename, O_RDONLY);
+    if (file_fd ==-1) {
+        perror("couldn't open(): ");
+        client_data->stat = ERR;
+    }
+}
+
+/**--------------------RESPONSE HANDLERS------------*/
 void send_ok(connection_t * client_data) {
     char * ok = "OK\n";
 
@@ -609,18 +659,20 @@ void send_ok(connection_t * client_data) {
         switch(client_data->method) {
             case GET:
                 client_data->stat = SEND_FS;
+                break;
             case PUT:
                 client_data->stat = CLOSED;
+                break;
             case DELETE:
                 client_data->stat = CLOSED;
+                break;
             case LIST:
                 client_data->stat = DATA_WRITE;
+                break;
             default:
                 break;
         }
     }
-
-    client_data->stat = CLOSED;
 }
 
 void send_err(connection_t * client_data) {
@@ -642,18 +694,19 @@ void send_err(connection_t * client_data) {
                 strcpy(err_msg, err_no_such_file);
                 break;
             default:
+                LOG("error not otherwise specified");
                 break;
-            
         }
     }
 
-    LOG("err_msg: %s", err_msg);
+    //LOG("err_msg: %s", err_msg);
     retval = write_to_socket(client_data, err_msg, strlen(err_msg));
-    LOG("wrote full errmsg: %d", (int)strlen(err_msg) == retval);
+    //LOG("wrote full errmsg: %d", (int)strlen(err_msg) == retval);
     if (retval == (int)strlen(err_msg)) {
         client_data->stat = CLOSED;
     }
 }
+
 /**-----------STATE MANAGEMENT-----------*/
 void state_switch(connection_t * client_data) {
     switch(client_data->stat) {
@@ -675,9 +728,11 @@ void state_switch(connection_t * client_data) {
                 handle_put_request(client_data);
                 break;
             case RES_HEADER:
-               send_ok(client_data);
+                send_ok(client_data);
                break;
             case SEND_FS:
+                send_fs(client_data);
+                break;
             case DATA_WRITE:
                 break;
             case ERR_REQ:
@@ -692,14 +747,17 @@ void state_switch(connection_t * client_data) {
             case ERR:
                 send_err(client_data);
                 break;
+            case CLOSED:
+                close_connection(client_data);
+                break;
             default:
                 break;
         }
 }
 
-void ready_dispatch(struct epoll_event * events) {
+void ready_dispatch(struct epoll_event * events, int ready_fds) {
     int idx = 0;
-    for (; idx < clientCount; idx++) {
+    for (; idx < ready_fds; idx++) {
         struct epoll_event event = events[idx];
         connection_t * client_data = (connection_t *)(event.data.ptr);
         state_switch(client_data);
@@ -716,7 +774,7 @@ void run_server() {
                 perror("epoll_wait():");
                 exit_fail();
             } else if (ready_fds > 0) {
-                ready_dispatch(events);
+                ready_dispatch(events, ready_fds);
             }
         }
         
@@ -745,7 +803,7 @@ int main(int argc, char **argv) {
     file_list = string_vector_create();
 
     //setup connection list vector
-    connections = int_to_shallow_dictionary_create();
+    connections = shallow_vector_create();
     
     //create temp dir
     char template[7] = "XXXXXX";
